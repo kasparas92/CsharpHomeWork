8815e6263b685991484a55211ef5289121b5d8ea
VILNIUS\kasparas.jurkevicius <kasparas.jurkevicius@euromonitor.com>
2020-09-17 16:23:08 +0300

    LINQ 18th August

---
+++ HomeWork/LinqExamples/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = Student.GetStudents();
+            var meritStudents = Student.GetMeritStudent();
+
+            Console.WriteLine("1.  Fetch StudentName who are not part of Merit List. ");
+            var studentsWhoAreNotPartOfMerit = students.GroupJoin(meritStudents, std => std.ID, mrt => mrt.ID, (x, y) => new { students = x, meritStudents = y })
+                                .SelectMany(x => x.meritStudents.DefaultIfEmpty(),
+                                            (x, y) => new { x.students, MeritStudentName = y?.Name ?? String.Empty})
+                                .Where(x => x.MeritStudentName == String.Empty)
+                                .Select(x => x.students.Name);
+
+            foreach(var v in studentsWhoAreNotPartOfMerit)
+            {
+                Console.WriteLine($"Student Name: {v}");
+            }
+
+            Console.WriteLine("2.  Fetch StudentDetails which are part of Student1 Object and Not Student Object. ");
+
+            Console.WriteLine("3.  Fetch Record of Students Whose Total marks > 265. ");
+
+            var studentRecords = students.Where(x => x.TotalMarks > 265);
+
+            foreach (var v in studentRecords)
+            {
+                Console.WriteLine($"ID: {v.ID}, {v.Name}, {v.TotalMarks}:");
+                foreach (var s in v.Subjects)
+                {
+                    Console.Write($"SubjectName: {s.SubjectName}, Mark: {s.Marks}");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("4.  Fetch Student Record whose marks in each Subject > 80. ");
+
+            var marksBiggerThan80 = students.Select(x => new { x.ID, x.Name, x.Subjects, x.TotalMarks, IfAllMarksBiggerThan80 = x.Subjects.All(m => m.Marks > 80) })
+                                            .Where(x => x.IfAllMarksBiggerThan80);
+
+            foreach (var v in marksBiggerThan80)
+            {
+                Console.WriteLine($"ID: {v.ID}, {v.Name}, {v.TotalMarks}:");
+                foreach (var s in v.Subjects)
+                {
+                    Console.Write($"SubjectName: {s.SubjectName}, Mark: {s.Marks}");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork/LinqExamples/Student.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExamples
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int TotalMarks { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public static List<Student> GetStudents()
+        {
+            List<Student> stud1 = new List<Student>();
+            stud1.Add(new Student
+            {
+                ID = 101,
+                Name = "Preety",
+                TotalMarks = 255,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 80},
+                        new Subject(){SubjectName = "Science", Marks = 90},
+                        new Subject(){SubjectName = "English", Marks = 95}
+                    }
+            });
+            stud1.Add(new Student
+            {
+                ID = 102,
+                Name = "Sambit",
+                TotalMarks = 275,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 90},
+                        new Subject(){SubjectName = "Science", Marks = 95},
+                        new Subject(){SubjectName = "English", Marks = 93}
+                }
+            });
+
+
+            stud1.Add(new Student
+            {
+                ID = 103,
+                Name = "Hina",
+                TotalMarks = 240,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 70},
+                        new Subject(){SubjectName = "Science", Marks = 80},
+                        new Subject(){SubjectName = "English", Marks = 90}
+                    }
+            });
+            stud1.Add(new Student
+            {
+                ID = 104,
+                Name = "Anurag",
+                TotalMarks = 278,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 90},
+                        new Subject(){SubjectName = "Science", Marks = 90},
+                        new Subject(){SubjectName = "English", Marks = 95}
+                    }
+            });
+            stud1.Add(new Student
+            {
+                ID = 105,
+                Name = "Pranaya",
+                TotalMarks = 265,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 80},
+                        new Subject(){SubjectName = "Science", Marks = 90},
+                        new Subject(){SubjectName = "English", Marks = 85}
+                    }
+            });
+            stud1.Add(new Student
+            {
+                ID = 106,
+                Name = "Santosh",
+                TotalMarks = 263,
+                Subjects = new List<Subject>()
+                    {
+                        new Subject(){SubjectName = "Math", Marks = 86},
+                        new Subject(){SubjectName = "Science", Marks = 70},
+                        new Subject(){SubjectName = "English", Marks = 95}
+                    }
+            });
+
+            return stud1;
+        }
+
+        public static List<Student> GetMeritStudent()
+        {
+            List<Student> stud = new List<Student>();
+            stud.Add(new Student { ID = 102, Name = "Sambit" });
+            stud.Add(new Student { ID = 104, Name = "Anurag" });
+            stud.Add(new Student { ID = 105, Name = "Pranaya" });
+            return stud;
+        }
+
+
+    }
+}
